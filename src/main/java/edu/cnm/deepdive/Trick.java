446c9d38ad05194ed11c37350e98057c24d433b7
Todd Nordquist <toddnordquist144@gmail.com>
2022-03-08 09:06:44 -0700

    Add color enum.

---
+++ src/main/java/edu/cnm/deepdive/Trick.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+public class Trick {
+
+  public static void main(String[] args) {
+
+    // TODO Create a shuffled deck.
+    // TODO Sort the deck into two piles, based on the rules of the trick.
+    // TODO Swap a random number of cards between the two piles.
+    // TODO Sort the piles.
+    // TODO Count & print out the cards and the count.
+  }
+}
